89ed81609d2a72ce739953f1e2a0c038c2c67854
redageddon <therealrubiksmaster@gmail.com>
2025-11-23 17:30:06 -0800

    added dataannotation attributes to RecipeModel

--- API/DataModels/RecipesModel.cs
+++ API/DataModels/RecipesModel.cs
@@ -1,20 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DataModels;
 
 public record RecipeModel(
     int Id,
-    string Name,
-    List<string> Ingredients,
-    List<string> Instructions,
-    double PrepTimeMinutes,
-    double CookTimeMinutes,
-    double Servings,
-    string Difficulty,
-    string Cuisine,
-    double CaloriesPerServing,
-    List<string> Tags,
     int UserId,
-    string Image,
-    double Rating,
-    int ReviewCount,
-    List<string> MealType
-);
+    [Required][MinLength(1)] string Name,
+    [Required][MinLength(1)] string Difficulty,
+    [Required][MinLength(1)] string Cuisine,
+    [Required] List<string> Ingredients,
+    [Required] List<string> Instructions,
+    [Required] List<string> Tags,
+    [Required] List<string> MealType,
+    [Required] string Image,
+    [Range(0, double.MaxValue)] double PrepTimeMinutes,
+    [Range(0, double.MaxValue)] double CookTimeMinutes,
+    [Range(0, double.MaxValue)] double Servings,
+    [Range(0, double.MaxValue)] double CaloriesPerServing,
+    [Range(0, int.MaxValue)] int ReviewCount,
+    [Range(0, 5)] double Rating);
