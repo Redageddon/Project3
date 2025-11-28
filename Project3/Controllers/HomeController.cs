bbf5cb18398c5189095583cee850e16a1b46e01a
Christian Rodriguez <crodriguez4643@gmail.com>
2025-11-28 14:42:45 -0800

    Add meals files incomplete

--- Project3/Controllers/HomeController.cs
+++ Project3/Controllers/HomeController.cs
@@ -11,11 +11,6 @@ public class HomeController(ILogger<HomeController> logger) : Controller
         return this.View();
     }
 
-    public IActionResult Privacy()
-    {
-        return this.View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- Project3/Controllers/MealsController.cs
+++ Project3/Controllers/MealsController.cs
@@ -1,18 +1,31 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using API.DataModels.Food;
+using Microsoft.AspNetCore.Mvc;
 using Project3.Models;
 using Project3.Services;
 
 namespace Project3.Controllers;
 
-public class MealsController(ILogger<MealsController> logger, RecipeApiService recipeApiService) : Controller
+public class MealsController(ILogger<MealsController> logger, MealsApiService mealsApiService) : Controller
 {
     public async Task<IActionResult> Index()
     {
-        RecipesDataModel recipesData = await recipeApiService.GetRecipes();
+        MealsDataModel mealsData = await mealsApiService.GetAllMeals();
+        
+        return this.View(mealsData);
+    }
 
-        return this.View(recipesData);
+    [Route("Meals/{id:int}")]
+    public async Task<IActionResult> MealsIndividual(int id)
+    {
+        MealsModel? meal = await mealsApiService.GetMealById(id);
+
+        if (meal == null)
+        {
+            return this.NotFound();
+        }
+
+        return this.View(meal);
     }
     
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
